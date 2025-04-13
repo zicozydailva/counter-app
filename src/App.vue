766d65dd2dcfdec26a601111980ce4f824655491
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-04-13 12:01:16 +0100

    chore: increment and decrement counts

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <div class="app">
-    <nav>
-      <RouterLink to="/">Home</RouterLink> |
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </div>
-
-  <RouterView />
+  <main>
+    <div class="app">
+      <nav>
+        <RouterLink to="/">Home</RouterLink> |
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+    <RouterView />
+  </main>
 </template>
 
 <style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div class="count">{{ count }}</div>
+    <div class="buttons">
+      <button @click="decrement">-</button>
+      <button @click="increment">+</button>
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const increment = () => {
+  count.value++;
+};
+
+const decrement = () => {
+  count.value--;
+};
+</script>
+
+
+<style>
+.home {
+  text-align: center;
+  margin-top: 60px;
+}
+
+.count {
+  font-size: 60px;
+  margin: 20px;
+}
+
+.buttons button {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>

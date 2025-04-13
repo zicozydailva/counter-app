ed92f54f79a0bf693e731f83c4fddf14904f6c3b
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-04-13 11:50:39 +0100

    chore: app setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="app">
+    <nav>
+      <RouterLink to="/">Home</RouterLink> |
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div>
+
+  <RouterView />
+</template>
+
+<style>
+.app {
+  text-align: center;
+}
+</style>
